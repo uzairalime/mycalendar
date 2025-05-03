947097a323c0dda2b2b64d090bd10049cc4ff053
Uzair Ali <uzairali.me@gmail.com>
2025-05-03 15:01:03 +0500

    issue in redirecting

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:get/get.dart';
@@ -10,59 +12,46 @@ import '../service/google_auth_client.dart';
 
 class AuthController extends GetxController{
 
-  final FlutterAppAuth _appAuth = const FlutterAppAuth();
+  // final FlutterAppAuth _appAuth = const FlutterAppAuth();
+  //
+  // final isMicrosoftLoggedIn = false.obs;
+  // final microsoftAccessToken = ''.obs;
+  // final microsoftRefreshToken = ''.obs;
 
-  final isMicrosoftLoggedIn = false.obs;
-  final microsoftAccessToken = ''.obs;
-  final microsoftRefreshToken = ''.obs;
-
-  final String clientId = AppConstant.clientIdMicrosoft;
-  // final String redirectUrl = 'com.example.mycalender://oauthredirect';
-  // final String redirectUrl = 'msauth://com.example.mycalender/VzSiQcXRmi2kyjzcA%2BmYLEtbGVs%3D';
-  final String redirectUrl = AppConstant.redirectUrl;
-  final String tenantId = AppConstant.tencentIdMicrosoft;
-  final List<String> scopes = [
-    'openid',
-    'profile',
+  // Initialize the plugin
+  final _appAuth = FlutterAppAuth();
+  final String _clientId     = AppConstant.clientIdMicrosoft;
+  final String _redirectUri  = AppConstant.redirectUrl;
+  final String _issuer       = 'https://login.microsoftonline.com/${AppConstant.tencentIdMicrosoft}';
+  final List<String> _scopes = [
+    'User.Read',
     'offline_access',
     'Calendars.ReadWrite'
   ];
 
-  Future<void> signInWithMicrosoft() async {
-    try {
-      // final result = await _appAuth.authorizeAndExchangeCode(
-      //   AuthorizationTokenRequest(
-      //     clientId,
-      //     redirectUrl,
-      //     discoveryUrl:
-      //     'https://login.microsoftonline.com/$tenantId/v2.0/.well-known/openid-configuration',
-      //     scopes: scopes,
-      //   ),
-      // );
-      final result = await _appAuth.authorizeAndExchangeCode(
+  Future<String?> signInWithMicrosoft() async {
+    log("-----------------Microsoft Sign In1");
+    try{
+      // Performs authorize + token exchange
+      final AuthorizationTokenResponse? result = await _appAuth.authorizeAndExchangeCode(
         AuthorizationTokenRequest(
-          clientId,
-          redirectUrl,
-          discoveryUrl: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
-          scopes: ['openid', 'profile', 'email', 'offline_access', 'Calendars.ReadWrite'],
+          _clientId,
+          _redirectUri,
+          issuer: _issuer,
+          scopes: ['User.Read', 'offline_access', 'Calendars.Read'],
+          // No additionalParameters or promptValues here if not needed
         ),
       );
-
-
       if (result != null) {
-        microsoftAccessToken.value = result.accessToken!;
-        microsoftRefreshToken.value = result.refreshToken!;
-        isMicrosoftLoggedIn.value = true;
+        final accessToken = result.accessToken;
+        // TODO: Store token and proceed to fetch calendar events
+        print('Access token: $accessToken');
       }
-    } catch (e) {
-      print('Microsoft login error: $e');
-    }
-  }
 
-  Future<void> signOutMicrosoft() async {
-    microsoftAccessToken.value = '';
-    microsoftRefreshToken.value = '';
-    isMicrosoftLoggedIn.value = false;
+    }catch(e){
+      log("-----------------Microsoft Sign In$e");
+      log("Microsoft Sign In Error: $e");
+    }
   }
 
 
